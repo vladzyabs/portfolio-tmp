880eb50342fada6b35a28889c5f7de5d300c36ce
vladzyabs <vladzyaba@gmail.com>
2020-09-03 16:46:26 +0300

    Updates

---
+++ precache-manifest.e003ca756c39f66608c27acb66bbeee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3cfd279722726c6728ffda95b2906e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f2b71abcc7bcc6ccc881",
+    "url": "/portfolio/static/css/main.6bee9405.chunk.css"
+  },
+  {
+    "revision": "dc6d961bb65fd82a9176",
+    "url": "/portfolio/static/js/2.4713a564.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4713a564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b71abcc7bcc6ccc881",
+    "url": "/portfolio/static/js/main.f3655a88.chunk.js"
+  },
+  {
+    "revision": "7e41f3906db46e1c6165",
+    "url": "/portfolio/static/js/runtime-main.d9ceefdd.js"
+  }
+]);
